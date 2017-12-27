0d6b13ee3538116ea373fe6ff429bacc1938bda8
HitarthChopra <hitarthchopra@gmail.com>
2017-12-27 14:00:54 +0530

    Update article.component.ts

--- app/article.component.ts
+++ app/article.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-
 import { ArticleService } from './article.service';
-import { SearchDataService } from './search-data.service'
-import { Article } from './article';
 
 @Component({
   selector: 'app-article',
@@ -18,6 +14,15 @@ tag:any;
 details:any;
 error:boolean = false;
 errorMsg:String = "";
+allArticles: any[];
+  
+  //Create constructor to get service instance
+  constructor(private articleService: ArticleService) {
+  }
+  //Create ngOnInit() and and load articles
+  ngOnInit(): void {
+    this.getAllArticles();
+  }
 
   searchData() {
 
@@ -55,7 +60,6 @@ errorMsg:String = "";
       this.articleService.deleteArticleById(this.id)
       .subscribe(success => {
         this.getAllArticles();
-        //this.backToCreateArticle();
         this.id="";
         this.value="";
         this.tag="";
@@ -141,58 +145,6 @@ errorMsg:String = "";
 
   }
 
-
-  data: any[];
-  searchResults: any[] = [];
-
-  //Create form
-  searchForm = new FormGroup({
-    value: new FormControl('', Validators.required),
-    id: new FormControl('', Validators.required),
-    tag: new FormControl('', Validators.required),
-    details: new FormControl('', Validators.required)
-  });
-
-
-
-  //Component properties
-  allArticles: any[];
-  articleIdToUpdate = null;
-  processValidation = false;
-  //Create form
-  articleForm = new FormGroup({
-    title: new FormControl('', Validators.required),
-    category: new FormControl('', Validators.required)
-  });
-  //Create constructor to get service instance
-  constructor(private articleService: ArticleService,
-    private searchDataService: SearchDataService) {
-  }
-  //Create ngOnInit() and and load articles
-  ngOnInit(): void {
-    this.getAllArticles();
-
-    this.data = this.searchDataService.data;
-  }
-
-  //Add data
-  // addData() {
-  //   if (this.searchForm.invalid) {
-  //     return; //Validation failed, exit from method.
-  //   }
-  //   //Form is valid, now perform create or update
-  //   let search = this.searchForm.value;
-  //   if (search.tag.includes("#")) {
-  //     this.searchDataService.data.push({
-  //       "title": search.value,
-  //       "tags": search.tag,
-  //       "highlights": "Angular is a platform that makes it easy to                                  build applications with the web.",
-  //       "detail": search.details,
-  //       "id": search.id
-  //     });
-  //   }
-  // }
-
   //Fetch all articles
   getAllArticles() {
     this.articleService.getAllArticles()
@@ -204,73 +156,4 @@ errorMsg:String = "";
     this.allArticles = data;
   }
 
-  //search
-  // searchData(){
-  //   console.log("value"+this.searchForm.value);
-  //   //console.log("id"+this.searchForm.id);
-  // }
-  //Handle create and update article
-  onArticleFormSubmit() {
-    this.processValidation = true;
-    if (this.articleForm.invalid) {
-      return; //Validation failed, exit from method.
-    }
-    //Form is valid, now perform create or update
-    let article = this.articleForm.value;
-    if (this.articleIdToUpdate === null) {
-      //Generate article id then create article
-      this.articleService.getAllArticles()
-        .subscribe(articles => {
-
-          //Generate article id	 
-          let maxIndex = articles.length - 1;
-          let articleWithMaxIndex = articles[maxIndex];
-          let articleId = articleWithMaxIndex.id + 1;
-          article.id = articleId;
-
-          //Create article
-          this.articleService.createArticle(article)
-            .subscribe(success => {
-              this.getAllArticles();
-              this.backToCreateArticle();
-            },
-            error => error
-            );
-        });
-    } else {
-      //Handle update article
-      article.id = this.articleIdToUpdate;
-      this.articleService.updateArticle(article)
-        .subscribe(success => {
-          this.getAllArticles();
-          this.backToCreateArticle();
-        },
-        error => error);
-    }
-  }
-  //Load article by id to edit
-  loadArticleToEdit(articleId: string) {
-    this.articleService.getArticleById(articleId)
-      .subscribe(article => {
-        this.articleIdToUpdate = article.id;
-        this.articleForm.setValue({ title: article.title, category: article.category });
-        this.processValidation = true;
-      },
-      error => error);
-  }
-  //Delete article
-  // deleteArticle(articleId: string) {
-  //   this.articleService.deleteArticleById(articleId)
-  //     .subscribe(success => {
-  //       this.getAllArticles();
-  //       this.backToCreateArticle();
-  //     },
-  //     error => error);
-  // }
-  //Go back from update to create
-  backToCreateArticle() {
-    this.articleIdToUpdate = null;
-    this.articleForm.reset();
-    this.processValidation = false;
-  }
 }
