a344c7a56f3e1fdb25779008d85d72e42fb69c22
HitarthChopra <hitarthchopra@gmail.com>
2017-12-27 14:10:49 +0530

    Update admin.component.ts

--- app/admin.component.ts
+++ app/admin.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ArticleService } from './article.service';
 
 @Component({
-  selector: 'app-article',
+  selector: 'admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
